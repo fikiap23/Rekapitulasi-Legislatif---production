eb09f60bb0e69b60f3c60b2fdb6d5d28080876bc
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-06 08:43:07 +0700

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,19 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import checker from 'vite-plugin-checker';
 
 // ----------------------------------------------------------------------
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+      },
+    }),
+  ],
   resolve: {
     alias: [
       {
